09df050ffc7c92940529b3b8696a04e930a8d0fe
lucasmss16 <lucasmss16@gmail.com>
2019-07-04 16:59:47 -0300

    update

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,12 +2,15 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Programa {
 
 	public static void main(String[] args) {
 	
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+	
 	}
 
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -9,7 +9,7 @@ public class Piece {
 		this.board = board;
 	}
 
-	public Board getBoard() {
+	protected Board getBoard() {
 		return board;
 	}
 	
