59dbba6e68ad65a0fc2d1f82578f196f12083358
lucasmss16 <lucasmss16@gmail.com>
2019-07-04 14:45:43 -0300

    update

---
+++ src/boardgame/Board.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Board {
+	
+	private int row;
+	private int column;
+	private Piece [][] pieces;
+	
+	public Board(int row, int column) {
+		this.row = row;
+		this.column = column;
+		pieces = new Piece[row][column];
+	}
+	
+	
+
+}
